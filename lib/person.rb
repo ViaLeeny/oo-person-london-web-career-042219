89165fbb941cac186734634a9bdc0fca0cffab44
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-24 22:44:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -75,11 +75,11 @@ class Person
   
   def start_conversation (person, topic)
     
-    if topic = "politics"
+    if topic == "politics"
       puts "blah blah partisan blah lobbyist"
        self.happiness += (-2)
        person.happiness += (-2)
-    elsif topic = "weather"
+    elsif topic == "weather"
       self.happiness += 1
       person.happiness += 1
       puts "blah blah sun blah rain"
