bad4df1f89080c34a9ede3616fac05e09482439a
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-24 21:59:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,58 @@
-# your code goes here
+#Create a Person class
+
+class Person 
+
+#Make it so name cannot be changed
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+#initialize the attributes we MUST define
+  def initialize(name)
+    @name = name 
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(amount)
+    if amount > 10 
+      amount = 10 
+    elsif amount< 0
+      amount = 0 
+    end
+    @happiness = amount
+  end
+  
+  
+ def hygiene=(amount)
+    if amount > 10 
+      amount = 10 
+    elsif amount< 0
+      amount = 0 
+    end
+    @hygiene = amount
+  end
+  
+  
+  def happy?
+    @happiness > 7
+  end 
+  
+  def clean?
+    @hygiene > 7
+  end 
+  
+  def get_paid(salary)
+    @bank_account += salary
+    return "all about the benjamins"
+  end 
+    
+  
+  def take_bath
+    @hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+    
+  end 
+    
+  
+end 
